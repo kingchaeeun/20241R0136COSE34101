5d8bcdc119846a0952259113d27690fa802c7a55
kingchaeeun <165105976+kingchaeeun@users.noreply.github.com>
2024-06-05 10:13:32 +0900

    Add files via upload

--- cpu_scheduling_simulator.c
+++ cpu_scheduling_simulator.c
@@ -23,6 +23,7 @@ typedef struct {
 // Function prototypes
 void generate_processes(Process processes[], int num_processes);
 void print_processes(Process processes[], int num_processes);
+void calculate_average_times(Process processes[], int num_processes);
 void fcfs_scheduling(Process processes[], int num_processes);
 void non_preemptive_sjf(Process processes[], int num_processes);
 void preemptive_sjf(Process processes[], int num_processes);
@@ -30,7 +31,7 @@ void non_preemptive_priority(Process processes[], int num_processes);
 void preemptive_priority(Process processes[], int num_processes);
 void round_robin(Process processes[], int num_processes, int time_quantum);
 void reset_processes(Process processes[], int num_processes);
-void print_gantt_chart(Process processes[], int num_processes, int completion_times[]);
+void print_gantt_chart(Process processes[], int num_processes, int completion_times[], int timeline[], int timeline_size);
 
 void generate_processes(Process processes[], int num_processes) {
     srand(time(NULL));
@@ -55,12 +56,25 @@ void print_processes(Process processes[], int num_processes) {
     }
 }
 
+void calculate_average_times(Process processes[], int num_processes) {
+    int total_waiting_time = 0;
+    int total_turnaround_time = 0;
+
+    for (int i = 0; i < num_processes; i++) {
+        total_waiting_time += processes[i].waiting_time;
+        total_turnaround_time += processes[i].turnaround_time;
+    }
+
+    printf("Average Waiting Time: %.2f\n", (float)total_waiting_time / num_processes);
+    printf("Average Turnaround Time: %.2f\n", (float)total_turnaround_time / num_processes);
+}
+
 void fcfs_scheduling(Process processes[], int num_processes) {
     printf("\nFCFS Scheduling:\n");
     int current_time = 0;
-    int total_waiting_time = 0;
-    int total_turnaround_time = 0;
     int completion_times[MAX_PROCESSES];
+    int timeline[100];
+    int timeline_size = 0;
 
     for (int i = 0; i < num_processes; i++) {
         if (current_time < processes[i].arrival_time) {
@@ -70,28 +84,27 @@ void fcfs_scheduling(Process processes[], int num_processes) {
         processes[i].turnaround_time = processes[i].waiting_time + processes[i].burst_time;
         processes[i].completion_time = current_time + processes[i].burst_time;
 
-        total_waiting_time += processes[i].waiting_time;
-        total_turnaround_time += processes[i].turnaround_time;
-
         current_time += processes[i].burst_time;
         completion_times[i] = current_time;
+
+        for (int j = 0; j < processes[i].burst_time; j++) {
+            timeline[timeline_size++] = processes[i].pid;
+        }
 
         printf("Process %d - Waiting Time: %d, Turnaround Time: %d\n", processes[i].pid, processes[i].waiting_time, processes[i].turnaround_time);
     }
 
-    print_gantt_chart(processes, num_processes, completion_times);
-
-    printf("Average Waiting Time: %.2f\n", (float)total_waiting_time / num_processes);
-    printf("Average Turnaround Time: %.2f\n", (float)total_turnaround_time / num_processes);
+    print_gantt_chart(processes, num_processes, completion_times, timeline, timeline_size);
+    calculate_average_times(processes, num_processes);
 }
 
 void non_preemptive_sjf(Process processes[], int num_processes) {
     printf("\nNon-Preemptive SJF Scheduling:\n");
     int current_time = 0;
     int completed = 0;
-    int total_waiting_time = 0;
-    int total_turnaround_time = 0;
     int completion_times[MAX_PROCESSES];
+    int timeline[100];
+    int timeline_size = 0;
 
     while (completed != num_processes) {
         int idx = -1;
@@ -109,13 +122,14 @@ void non_preemptive_sjf(Process processes[], int num_processes) {
             processes[idx].turnaround_time = processes[idx].waiting_time + processes[idx].burst_time;
             processes[idx].completion_time = current_time + processes[idx].burst_time;
 
-            total_waiting_time += processes[idx].waiting_time;
-            total_turnaround_time += processes[idx].turnaround_time;
-
             current_time += processes[idx].burst_time;
             processes[idx].completed = true;
             completed++;
             completion_times[completed - 1] = current_time;
+
+            for (int j = 0; j < processes[idx].burst_time; j++) {
+                timeline[timeline_size++] = processes[idx].pid;
+            }
 
             printf("Process %d - Waiting Time: %d, Turnaround Time: %d\n", processes[idx].pid, processes[idx].waiting_time, processes[idx].turnaround_time);
         }
@@ -124,20 +138,17 @@ void non_preemptive_sjf(Process processes[], int num_processes) {
         }
     }
 
-    print_gantt_chart(processes, num_processes, completion_times);
-
-    printf("Average Waiting Time: %.2f\n", (float)total_waiting_time / num_processes);
-    printf("Average Turnaround Time: %.2f\n", (float)total_turnaround_time / num_processes);
+    print_gantt_chart(processes, num_processes, completion_times, timeline, timeline_size);
+    calculate_average_times(processes, num_processes);
 }
 
 void preemptive_sjf(Process processes[], int num_processes) {
     printf("\nPreemptive SJF Scheduling:\n");
     int current_time = 0;
     int completed = 0;
-    int total_waiting_time = 0;
-    int total_turnaround_time = 0;
-    int prev = -1;
     int completion_times[MAX_PROCESSES];
+    int timeline[100];
+    int timeline_size = 0;
 
     while (completed != num_processes) {
         int idx = -1;
@@ -151,11 +162,8 @@ void preemptive_sjf(Process processes[], int num_processes) {
         }
 
         if (idx != -1) {
-            if (prev != -1 && prev != idx) {
-                printf("Process %d executed for %d time units.\n", processes[prev].pid, current_time - processes[prev].arrival_time);
-            }
-
             processes[idx].remaining_time--;
+            timeline[timeline_size++] = processes[idx].pid;
             current_time++;
 
             if (processes[idx].remaining_time == 0) {
@@ -165,32 +173,27 @@ void preemptive_sjf(Process processes[], int num_processes) {
                 processes[idx].turnaround_time = current_time - processes[idx].arrival_time;
                 processes[idx].completion_time = current_time;
 
-                total_waiting_time += processes[idx].waiting_time;
-                total_turnaround_time += processes[idx].turnaround_time;
                 completion_times[completed - 1] = current_time;
 
                 printf("Process %d - Waiting Time: %d, Turnaround Time: %d\n", processes[idx].pid, processes[idx].waiting_time, processes[idx].turnaround_time);
             }
-            prev = idx;
         }
         else {
             current_time++;
         }
     }
 
-    print_gantt_chart(processes, num_processes, completion_times);
-
-    printf("Average Waiting Time: %.2f\n", (float)total_waiting_time / num_processes);
-    printf("Average Turnaround Time: %.2f\n", (float)total_turnaround_time / num_processes);
+    print_gantt_chart(processes, num_processes, completion_times, timeline, timeline_size);
+    calculate_average_times(processes, num_processes);
 }
 
 void non_preemptive_priority(Process processes[], int num_processes) {
     printf("\nNon-Preemptive Priority Scheduling:\n");
     int current_time = 0;
     int completed = 0;
-    int total_waiting_time = 0;
-    int total_turnaround_time = 0;
     int completion_times[MAX_PROCESSES];
+    int timeline[100];
+    int timeline_size = 0;
 
     while (completed != num_processes) {
         int idx = -1;
@@ -208,13 +211,14 @@ void non_preemptive_priority(Process processes[], int num_processes) {
             processes[idx].turnaround_time = processes[idx].waiting_time + processes[idx].burst_time;
             processes[idx].completion_time = current_time + processes[idx].burst_time;
 
-            total_waiting_time += processes[idx].waiting_time;
-            total_turnaround_time += processes[idx].turnaround_time;
-
             current_time += processes[idx].burst_time;
             processes[idx].completed = true;
             completed++;
             completion_times[completed - 1] = current_time;
+
+            for (int j = 0; j < processes[idx].burst_time; j++) {
+                timeline[timeline_size++] = processes[idx].pid;
+            }
 
             printf("Process %d - Waiting Time: %d, Turnaround Time: %d\n", processes[idx].pid, processes[idx].waiting_time, processes[idx].turnaround_time);
         }
@@ -223,20 +227,17 @@ void non_preemptive_priority(Process processes[], int num_processes) {
         }
     }
 
-    print_gantt_chart(processes, num_processes, completion_times);
-
-    printf("Average Waiting Time: %.2f\n", (float)total_waiting_time / num_processes);
-    printf("Average Turnaround Time: %.2f\n", (float)total_turnaround_time / num_processes);
+    print_gantt_chart(processes, num_processes, completion_times, timeline, timeline_size);
+    calculate_average_times(processes, num_processes);
 }
 
 void preemptive_priority(Process processes[], int num_processes) {
     printf("\nPreemptive Priority Scheduling:\n");
     int current_time = 0;
     int completed = 0;
-    int total_waiting_time = 0;
-    int total_turnaround_time = 0;
-    int prev = -1;
     int completion_times[MAX_PROCESSES];
+    int timeline[100];
+    int timeline_size = 0;
 
     while (completed != num_processes) {
         int idx = -1;
@@ -250,11 +251,8 @@ void preemptive_priority(Process processes[], int num_processes) {
         }
 
         if (idx != -1) {
-            if (prev != -1 && prev != idx) {
-                printf("Process %d executed for %d time units.\n", processes[prev].pid, current_time - processes[prev].arrival_time);
-            }
-
             processes[idx].remaining_time--;
+            timeline[timeline_size++] = processes[idx].pid;
             current_time++;
 
             if (processes[idx].remaining_time == 0) {
@@ -264,64 +262,72 @@ void preemptive_priority(Process processes[], int num_processes) {
                 processes[idx].turnaround_time = current_time - processes[idx].arrival_time;
                 processes[idx].completion_time = current_time;
 
-                total_waiting_time += processes[idx].waiting_time;
-                total_turnaround_time += processes[idx].turnaround_time;
                 completion_times[completed - 1] = current_time;
 
                 printf("Process %d - Waiting Time: %d, Turnaround Time: %d\n", processes[idx].pid, processes[idx].waiting_time, processes[idx].turnaround_time);
             }
-            prev = idx;
         }
         else {
             current_time++;
         }
     }
 
-    print_gantt_chart(processes, num_processes, completion_times);
-
-    printf("Average Waiting Time: %.2f\n", (float)total_waiting_time / num_processes);
-    printf("Average Turnaround Time: %.2f\n", (float)total_turnaround_time / num_processes);
+    print_gantt_chart(processes, num_processes, completion_times, timeline, timeline_size);
+    calculate_average_times(processes, num_processes);
 }
 
 void round_robin(Process processes[], int num_processes, int time_quantum) {
-    printf("\nRound Robin Scheduling (Time Quantum: %d):\n", time_quantum);
+    printf("\nRound Robin Scheduling:\n");
     int current_time = 0;
-    int total_waiting_time = 0;
-    int total_turnaround_time = 0;
     int completed = 0;
     int completion_times[MAX_PROCESSES];
-    int time_slice[MAX_PROCESSES] = { 0 };
+    int timeline[100];
+    int timeline_size = 0;
+    int queue[100];
+    int front = 0, rear = 0;
+
+    for (int i = 0; i < num_processes; i++) {
+        queue[rear++] = i;
+    }
 
     while (completed != num_processes) {
-        for (int i = 0; i < num_processes; i++) {
-            if (processes[i].arrival_time <= current_time && processes[i].remaining_time > 0) {
-                if (processes[i].remaining_time > time_quantum) {
-                    current_time += time_quantum;
-                    processes[i].remaining_time -= time_quantum;
+        int idx = queue[front++];
+        if (processes[idx].remaining_time > 0 && processes[idx].arrival_time <= current_time) {
+            if (processes[idx].remaining_time <= time_quantum) {
+                current_time += processes[idx].remaining_time;
+                processes[idx].remaining_time = 0;
+                processes[idx].completed = true;
+                completed++;
+                processes[idx].waiting_time = current_time - processes[idx].arrival_time - processes[idx].burst_time;
+                processes[idx].turnaround_time = current_time - processes[idx].arrival_time;
+                processes[idx].completion_time = current_time;
+
+                completion_times[completed - 1] = current_time;
+
+                for (int j = 0; j < processes[idx].burst_time; j++) {
+                    timeline[timeline_size++] = processes[idx].pid;
                 }
-                else {
-                    current_time += processes[i].remaining_time;
-                    processes[i].waiting_time = current_time - processes[i].arrival_time - processes[i].burst_time;
-                    processes[i].turnaround_time = current_time - processes[i].arrival_time;
-                    processes[i].remaining_time = 0;
-                    processes[i].completion_time = current_time;
-                    completed++;
 
-                    total_waiting_time += processes[i].waiting_time;
-                    total_turnaround_time += processes[i].turnaround_time;
-
-                    completion_times[completed - 1] = current_time;
-
-                    printf("Process %d - Waiting Time: %d, Turnaround Time: %d\n", processes[i].pid, processes[i].waiting_time, processes[i].turnaround_time);
-                }
+                printf("Process %d - Waiting Time: %d, Turnaround Time: %d\n", processes[idx].pid, processes[idx].waiting_time, processes[idx].turnaround_time);
             }
+            else {
+                current_time += time_quantum;
+                processes[idx].remaining_time -= time_quantum;
+
+                for (int j = 0; j < time_quantum; j++) {
+                    timeline[timeline_size++] = processes[idx].pid;
+                }
+
+                queue[rear++] = idx;
+            }
+        }
+        else {
+            queue[rear++] = idx;
         }
     }
 
-    print_gantt_chart(processes, num_processes, completion_times);
-
-    printf("Average Waiting Time: %.2f\n", (float)total_waiting_time / num_processes);
-    printf("Average Turnaround Time: %.2f\n", (float)total_turnaround_time / num_processes);
+    print_gantt_chart(processes, num_processes, completion_times, timeline, timeline_size);
+    calculate_average_times(processes, num_processes);
 }
 
 void reset_processes(Process processes[], int num_processes) {
@@ -333,33 +339,33 @@ void reset_processes(Process processes[], int num_processes) {
     }
 }
 
-void print_gantt_chart(Process processes[], int num_processes, int completion_times[]) {
+void print_gantt_chart(Process processes[], int num_processes, int completion_times[], int timeline[], int timeline_size) {
     printf("\nGantt Chart:\n");
     printf(" |");
 
-    for (int i = 0; i < num_processes; i++) {
-        for (int j = 0; j < processes[i].burst_time; j++) {
-            printf("--");
-        }
-        printf("P%d|", processes[i].pid);
+    for (int i = 0; i < timeline_size; i++) {
+        printf("P%d|", timeline[i]);
     }
 
     printf("\n0");
 
-    for (int i = 0; i < num_processes; i++) {
-        for (int j = 0; j < processes[i].burst_time; j++) {
-            printf("  ");
+    int time = 0;
+    for (int i = 0; i < timeline_size; i++) {
+        time++;
+        if (time < 10) {
+            printf("   %d", time);
         }
-        if (completion_times[i] > 9) {
-            printf("\b");
+        else {
+            printf("  %d", time);
         }
-        printf("%d", completion_times[i]);
     }
     printf("\n");
 }
 
 int main() {
+    Process processes[MAX_PROCESSES];
     int num_processes;
+
     printf("Enter the number of processes: ");
     scanf("%d", &num_processes);
 
@@ -368,42 +374,26 @@ int main() {
         return 1;
     }
 
-    Process processes[MAX_PROCESSES];
     generate_processes(processes, num_processes);
     print_processes(processes, num_processes);
 
-    // FCFS Scheduling
     fcfs_scheduling(processes, num_processes);
-
-    // Reset processes
     reset_processes(processes, num_processes);
 
-    // Non-Preemptive SJF Scheduling
     non_preemptive_sjf(processes, num_processes);
-
-    // Reset processes
     reset_processes(processes, num_processes);
 
-    // Preemptive SJF Scheduling
     preemptive_sjf(processes, num_processes);
-
-    // Reset processes
     reset_processes(processes, num_processes);
 
-    // Non-Preemptive Priority Scheduling
     non_preemptive_priority(processes, num_processes);
-
-    // Reset processes
     reset_processes(processes, num_processes);
 
-    // Preemptive Priority Scheduling
     preemptive_priority(processes, num_processes);
-
-    // Reset processes
     reset_processes(processes, num_processes);
 
-    // Round Robin Scheduling
     round_robin(processes, num_processes, TIME_QUANTUM);
+    reset_processes(processes, num_processes);
 
     return 0;
 }
